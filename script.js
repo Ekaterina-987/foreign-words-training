da167d34d9d3b31437740d3ce37d54fbaaba3058
Ekaterina-987 <77798912+Ekaterina-987@users.noreply.github.com>
2022-12-11 20:04:22 +0300

    exam

--- script.js
+++ script.js
@@ -63,38 +63,29 @@ doWorkoutMode();
 
 cards.addEventListener('click', function() {
     cards.classList.toggle('active');
-
 })
 
+
+let j = 1;
 buttonNext.addEventListener('click', function() {
-    currentWord.textContent = ++currentWord.textContent;
     buttonBack.removeAttribute('disabled');
-
-    for (let j = 0; j <= 5; j++) {
-        doWorkoutMode(object[j]);
-
-        if (j == 5) {
-            buttonNext.disabled = true;
-        }
-
-    }
-
-    /*if (currentWord === '5') {
+    if (j === 4) {
         buttonNext.disabled = true;
-    }*/
-
+        currentWord.textContent = ++j;
+    } else {
+        doWorkoutMode(object[j++]);
+        currentWord.textContent = j;
+    }
 })
 
 buttonBack.addEventListener('click', function() {
-    currentWord.textContent = --currentWord.textContent;
-    for (let j = 0; j >= 0; j--) {
-        doWorkoutMode(object[j - 1]);
-
-    }
-    if (j == 1) {
+    buttonNext.disabled = false;
+    if (j === 1) {
         buttonBack.disabled = true;
+    } else {
+        doWorkoutMode(object[--j]);
+        currentWord.textContent = j;
     }
-
 })
 
 buttonExam.addEventListener('click', function() {
@@ -114,8 +105,16 @@ buttonExam.addEventListener('click', function() {
         examCards.append(patternExamCards2);
 
     }
+    shuffle(examCards);
+    const fragment = new DocumentFragment();
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = object[i];
+    fragment.append(div);
+    document.body.append(fragment)
+        //examCards.innerHTML = shuffle(examCards)
 
-    timerId = setInterval(function() {
+    /*timerId = setInterval(function() {
             let minutes = time.textContent.split(":")[0];
             let seconds = time.textContent.split(":")[1];
 
@@ -134,34 +133,43 @@ buttonExam.addEventListener('click', function() {
             if (minutes < 10 && seconds == 00) {
                 time.textContent = `0${minutes}:${seconds}`;
             }
-        }, 100)
-        //alert(typeof examCards);
-
-    //examCards = examCards.sort(() => Math.random() - 0.5);
-    //console.log(examCards)
+        }, 100)*/
 
 })
 
-/*function p() {
-    examCards = examCards.sort(() => Math.random() - 0.5);
-    console.log(examCards)
+function shuffle(arr) {
+    arr.sort(() => Math.random() - 0.5);
 }
 
-p()*/
-//examCards = examCards.sort(() => Math.random() - 0.5);
-
-examCards.addEventListener('click', function(event) {
+/*let n = examCards.addEventListener('click', function(event) {
     let target = event.target;
     selectCard(target);
+    //examCards.classList.add('correct');
 
-})
+})*/
 
 function selectCard(card) {
-    card[0].classList.add('correct');
-    if (card[0] == card[1]) {
-        card[1].classList.add('correct');
-    } else {
-        card[1].classList.add('wrong');
-    }
+    card.classList.add('correct');
 
+}
+
+const dict = {}
+for (let elem of object) {
+    const { word, translation } = elem;
+    dict[word] = translation;
+    dict[translation] = word;
+    console.log(dict)
+}
+
+/*for (let m = 0; m <= examCards.length; m++) {
+    examCards[i].addEventListener('click', function() {
+        examCards.classList.add('correct');
+    })
+}*/
+
+//const car = document.querySelectorAll('div');
+for (let oneCard of examCards) {
+    oneCard.addEventListener('click', function() {
+        selectCard();
+    })
 }
